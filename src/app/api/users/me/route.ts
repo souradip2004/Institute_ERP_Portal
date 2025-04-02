2fd1bbb53be15ac46cdaccd8577513db7d87f556
Souradip Saha <22052939@kiit.ac.in>
2025-04-02 13:23:03 +0530

    Hash password for update profile

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/config/prisma";
 import { auth } from "@/auth";
+import bcrypt from "bcryptjs";
 
 // Handler for GET /users/me
 export async function GET() {
@@ -35,40 +36,53 @@ export async function GET() {
 
 // Handler for PUT /users/me
 export async function PUT(req: NextRequest) {
-    try {
-      // Get current authenticated user from session
-      const session = await auth();
-      if (!session?.user?.email) {
-        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-      }
-  
-      const email = session.user.email;
-      const updatedData = await req.json(); // Data to update
-  
-      // Prevent updating sensitive or restricted fields
-    //   const { id, email: newEmail, password, role, ...allowedUpdates } = updatedData;
-    const { id, ...allowedUpdates } = updatedData;
-  
-      // Process date fields if they exist
-      let processedUpdates = { ...allowedUpdates };
-      if (processedUpdates.dateOfBirth && typeof processedUpdates.dateOfBirth === 'string') {
-        processedUpdates.dateOfBirth = new Date(processedUpdates.dateOfBirth);
-      }
-  
-      // Update the user in the database based on email
-      const updatedUser = await prisma.user.update({
-        where: { email },
-        data: processedUpdates,
-      });
-  
-      // Remove sensitive information before returning
-      const { password: userPassword, ...safeUserData } = updatedUser;
-      return NextResponse.json(safeUserData);
-    } catch (error: any) {
-      console.error("Update user error:", error);
-      return NextResponse.json({ error: error.message }, { status: 500 });
+  try {
+    // Get current authenticated user from session
+    const session = await auth();
+    if (!session?.user?.email) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
+
+    const email = session.user.email;
+    const updatedData = await req.json(); // Data to update
+
+    // Prevent updating sensitive or restricted fields
+    //   const { id, email: newEmail, password, role, ...allowedUpdates } = updatedData;
+    const { id, email: newEmail, ...allowedUpdates } = updatedData;
+
+    // Process date fields if they exist
+    let processedUpdates = { ...allowedUpdates };
+    const { password } = { ...allowedUpdates };
+
+    if (
+      processedUpdates.password &&
+      typeof processedUpdates.password === "string"
+    ) {
+      processedUpdates.password = await bcrypt.hash(password, 10);
+    }
+
+    if (
+      processedUpdates.dateOfBirth &&
+      typeof processedUpdates.dateOfBirth === "string"
+    ) {
+      processedUpdates.dateOfBirth = new Date(processedUpdates.dateOfBirth);
+    }
+
+    console.log(processedUpdates);
+    // Update the user in the database based on email
+    const updatedUser = await prisma.user.update({
+      where: { email },
+      data: processedUpdates,
+    });
+
+    // Remove sensitive information before returning
+    const { password: userPassword, ...safeUserData } = updatedUser;
+    return NextResponse.json(safeUserData);
+  } catch (error: any) {
+    console.error("Update user error:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
+}
 
 // Handler for DELETE /users/me
 export async function DELETE() {
