4b97450e348e9073874a71edc3dcb08cb8e15ec9
Souradip Saha <22052939@kiit.ac.in>
2025-07-01 18:35:17 +0530

    merged

--- src/components/teacher/TeacherSidebar.tsx
+++ src/components/teacher/TeacherSidebar.tsx
@@ -37,7 +37,7 @@ export default function TeacherSidebar({ onSidebarToggle }: TeacherSidebarProps)
     if (localStorage.getItem("user")) {
       const data = JSON.parse(localStorage.getItem("user"))?.institutionId;
       const fetchInstitute = async () => {
-        const institutionRes = await fetch(`http://localhost:3000/api/institutions/${data}`, {
+        const institutionRes = await fetch(`https://commercial.aiclassroom.in/api/institutions/${data}`, {
           cache: "no-store",
         });
         let institutionData = await institutionRes.json();
